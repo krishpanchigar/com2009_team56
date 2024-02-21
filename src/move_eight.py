bf4e15c0881138ddc47cb5541e7f01cfcf1c09af
Krish Panchigar <krishpanchigar14@gmail.com>
2024-02-21 10:57:42 +0000

    some changes

--- src/move_eight.py
+++ src/move_eight.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+# TODO: make it turn and stop based on coordinates maybe
+
 
 import rospy
 from geometry_msgs.msg import Twist
@@ -13,6 +15,7 @@ class MoveEight:
         self.sub = rospy.Subscriber("odom", Odometry, self.callback)
         self.rate = rospy.Rate(10)  
         self.start_position = None
+        self.start_yaw = None
         self.current_position = None
         self.last_position = None
         self.total_distance = 0
@@ -25,6 +28,11 @@ class MoveEight:
         if self.start_position is None:
             self.start_position = position
             self.last_position = position
+        
+        if self.start_yaw is None:
+            (roll, pitch, yaw) = euler_from_quaternion([orientation.x, orientation.y, 
+                        orientation.z, orientation.w], "sxyz")
+            self.start_yaw = yaw * 180 / math.pi
 
         self.current_position = position 
         distance = math.sqrt((self.current_position.x - self.last_position.x) ** 2 +
@@ -32,14 +40,15 @@ class MoveEight:
         self.total_distance += distance
         self.last_position = self.current_position  
 
-        pos_x = position.x
-        pos_y = position.y
+        pos_x = position.x - self.start_position.x
+        pos_y = position.y - self.start_position.y
 
         (roll, pitch, yaw) = euler_from_quaternion([orientation.x, orientation.y, 
                         orientation.z, orientation.w], "sxyz")
         yaw_degrees = yaw * 180 / math.pi
+        yaw_degrees_adjusted = yaw_degrees - self.start_yaw
 
-        print(f"x={pos_x} [m], y={pos_y} [m], yaw={yaw_degrees} [degrees].")
+        print(f"x={pos_x: .2f} [m], y={pos_y: .2f} [m], yaw={yaw_degrees_adjusted: .2f} [degrees].")
 
     def move_eight(self, clockwise = False):
         twist = Twist()
@@ -51,7 +60,7 @@ class MoveEight:
             else:
                 twist.angular.z = -math.pi/15 if not clockwise else math.pi/15
 
-            if self.total_distance >= 2 * math.pi:
+            if self.total_distance > 2 * math.pi:
                 break
 
             self.pub.publish(twist)
