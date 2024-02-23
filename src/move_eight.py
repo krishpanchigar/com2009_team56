c487857e06fb9832038f0814d693b49671fe5bf8
Krish Panchigar <krishpanchigar14@gmail.com>
2024-02-23 15:02:17 +0000

    working on it!

--- src/move_eight.py
+++ src/move_eight.py
@@ -7,6 +7,7 @@ from geometry_msgs.msg import Twist
 import math
 from tf.transformations import euler_from_quaternion
 from nav_msgs.msg import Odometry
+import time
 
 class MoveEight:
     def __init__(self):
@@ -17,8 +18,12 @@ class MoveEight:
         self.start_position = None
         self.start_yaw = None
         self.current_position = None
+        self.current_yaw = None
         self.last_position = None
+        self.last_yaw = None
         self.total_distance = 0
+        self.first_circle = True
+        self.second_circle = False
     
     def callback(self, topic_data: Odometry):
         pose = topic_data.pose.pose
@@ -27,40 +32,43 @@ class MoveEight:
 
         if self.start_position is None:
             self.start_position = position
-            self.last_position = position
+            self.current_position = position
         
-        if self.start_yaw is None:
-            (roll, pitch, yaw) = euler_from_quaternion([orientation.x, orientation.y, 
-                        orientation.z, orientation.w], "sxyz")
-            self.start_yaw = yaw * 180 / math.pi
-
-        self.current_position = position 
-        distance = math.sqrt((self.current_position.x - self.last_position.x) ** 2 +
-                                 (self.current_position.y - self.last_position.y) ** 2)
-        self.total_distance += distance
-        self.last_position = self.current_position  
-
-        pos_x = position.x - self.start_position.x
-        pos_y = position.y - self.start_position.y
+        self.current_position.x = position.x - self.start_position.x
+        self.current_position.y = position.y - self.start_position.y
+        self.current_position.z = position.z - self.start_position.z
 
         (roll, pitch, yaw) = euler_from_quaternion([orientation.x, orientation.y, 
                         orientation.z, orientation.w], "sxyz")
+        
         yaw_degrees = yaw * 180 / math.pi
-        yaw_degrees_adjusted = yaw_degrees - self.start_yaw
 
-        print(f"x={pos_x: .2f} [m], y={pos_y: .2f} [m], yaw={yaw_degrees_adjusted: .1f} [degrees].")
+        if self.first_circle and self.current_position.x > 0.4:
+            self.first_circle = False
+
+        if not self.first_circle and self.current_position.x < -0.4:
+            self.second_circle = True
+
+        if self.start_yaw is None:
+            self.start_yaw = yaw * 180 / math.pi
+
+        self.current_yaw = yaw_degrees - self.start_yaw  
+
+        pos_x = position.x
+        pos_y = position.y
+
+        print(f"x={pos_x: .2f} [m], y={pos_y: .2f} [m], yaw={self.current_yaw: .1f} [degrees].")
 
     def move_eight(self, clockwise = False):
         twist = Twist()
         twist.linear.x = math.pi / 30
-
+    
         while not rospy.is_shutdown():
-            if self.total_distance < math.pi:
-                twist.angular.z = math.pi/15 if not clockwise else -math.pi/15
-            else:
-                twist.angular.z = -math.pi/15 if not clockwise else math.pi/15
-
-            if self.total_distance > 2 * math.pi:
+            if self.first_circle:
+                twist.angular.z = -math.pi / 15
+            elif not self.first_circle and not self.second_circle and self.current_position.x <= 0.05 and self.current_position.y <= 0.05 and self.current_yaw <= 1:
+                twist.angular.z = math.pi / 15
+            elif not self.first_circle and self.second_circle and self.current_position.x <= 0.05 and self.current_position.y <= 0.05 and self.current_yaw <= 1:
                 break
 
             self.pub.publish(twist)
