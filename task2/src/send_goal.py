5ef762429b2c8ca96892e90983aa5fc54a0b5ecd
Krish Panchigar <krishpanchigar14@gmail.com>
2024-03-08 12:13:04 +0000

    working

--- task2/src/send_goal.py
+++ task2/src/send_goal.py
@@ -2,41 +2,68 @@
 
 import rospy
 from geometry_msgs.msg import PoseStamped
+from nav_msgs.msg import Odometry
+from tf.transformations import euler_from_quaternion
+import math
+
+import time
 
 class SendGoal:
     def __init__(self):
         rospy.init_node('multi_point_navigator', anonymous=True)
+
+        self.last_print_time = rospy.Time.now() 
+        self.start_position = None
+
         self.pub = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=1)
+        self.sub = rospy.Subscriber("odom", Odometry, self.callback)
         rospy.sleep(1) 
         self.waypoints = [
-            (-0.5, -0.5),
-            (-1.5, -0.5),
-            (-1.5, 0.5),
-            (-1.5, 1.5),
-            (-0.5, 1.5),
-            (0.5, 1.5),
-            (1.5, 1.5),
-            (1.5, 0.5),
-            (1.5, -0.5),
-            (1.5, -1.5),
-            (0.5, -1.5),
-            (-1.5, -1.5)
-        ]
+            (-1.5, -0.5, math.pi / 2),
+            (-1.5, 0.5, math.pi / 2),
+            (-1.5, 1.5, math.pi),
+            (-0.5, 1.5, math.pi),
+            (0.5, 1.5, math.pi),
+            (1.5, 1.5, -math.pi / 2),
+            (1.5, 0.5, -math.pi / 2),
+            (1.5, -0.5, -math.pi / 2),
+            (1.5, -1.5, -math.pi),
+            (0.5, -1.5, -math.pi),
+            (-0.5, -1.5, -math.pi),
+            (-1.5, -1.5, math.pi)
+        ]                
+        
 
-    def send_goal(self, x, y):
+
+    def callback(self, topic_data: Odometry):
+        pose = topic_data.pose.pose
+        position = pose.position
+        orientation = pose.orientation
+
+        (roll, pitch, yaw) = euler_from_quaternion([orientation.x, orientation.y, 
+                        orientation.z, orientation.w], "sxyz")
+        pos_x = position.x
+        pos_y = position.y
+
+        current_time = rospy.Time.now()
+        if (current_time - self.last_print_time).to_sec() >= 1.0:
+            print(f"x={pos_x: .2f} [m], y={pos_y: .2f} [m], yaw={yaw: .1f} [degrees].")
+            self.last_print_time = current_time
+
+    def send_goal(self, x, y, z):
         goal = PoseStamped()
         goal.header.frame_id = "map"
         goal.header.stamp = rospy.Time.now()
         goal.pose.position.x = x
         goal.pose.position.y = y
-        goal.pose.orientation.w = 1.0
+        goal.pose.orientation.w = z
         self.pub.publish(goal)
-        rospy.loginfo(f"Sending goal to ({x}, {y})")
+        rospy.loginfo(f"Sending goal to ({x}, {y}, {z})")
 
     def navigate(self):
         for waypoint in self.waypoints:
-            x, y = waypoint
-            self.send_goal(x, y)
+            x, y, z = waypoint
+            self.send_goal(x, y, z)
             rospy.sleep(10)  
 
 if __name__ == "__main__":
