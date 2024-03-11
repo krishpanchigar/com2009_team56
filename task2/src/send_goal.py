ed4752c4ae01deb53b67a94040d5af61aaaa2e0a
Krish Panchigar <krishpanchigar14@gmail.com>
2024-03-11 11:13:00 +0000

    Getting goal status

--- task2/src/send_goal.py
+++ task2/src/send_goal.py
@@ -5,6 +5,7 @@ from geometry_msgs.msg import PoseStamped
 from nav_msgs.msg import Odometry
 from tf.transformations import euler_from_quaternion
 import math
+from move_base_msgs.msg import MoveBaseActionResult
 
 import time
 
@@ -14,10 +15,14 @@ class SendGoal:
 
         self.last_print_time = rospy.Time.now() 
         self.start_position = None
+        self.is_bot_ready = True
+        self.rate = rospy.Rate(10)
 
-        self.pub = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=1)
+        self.pub_goal = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=1)
+        self.sub_goal_result = rospy.Subscriber('/move_base/result', MoveBaseActionResult, self.goal_result_cb)
         self.sub = rospy.Subscriber("odom", Odometry, self.callback)
         rospy.sleep(1) 
+
         self.waypoints = [
             (-1.5, -0.5, math.pi / 2),
             (-1.5, 0.5, math.pi / 2),
@@ -34,7 +39,6 @@ class SendGoal:
         ]                
         
 
-
     def callback(self, topic_data: Odometry):
         pose = topic_data.pose.pose
         position = pose.position
@@ -50,21 +54,26 @@ class SendGoal:
             print(f"x={pos_x: .2f} [m], y={pos_y: .2f} [m], yaw={yaw: .1f} [degrees].")
             self.last_print_time = current_time
 
+    
+
+    
     def send_goal(self, x, y, z):
         goal = PoseStamped()
         goal.header.frame_id = "map"
         goal.header.stamp = rospy.Time.now()
+        goal.header.frame_id = 'map'
         goal.pose.position.x = x
         goal.pose.position.y = y
         goal.pose.orientation.w = z
-        self.pub.publish(goal)
+        self.pub_goal.publish(goal)
+        self.is_bot_ready = False
         rospy.loginfo(f"Sending goal to ({x}, {y}, {z})")
 
     def navigate(self):
         for waypoint in self.waypoints:
             x, y, z = waypoint
-            self.send_goal(x, y, z)
-            rospy.sleep(10)  
+            if self.is_bot_ready:
+                self.send_goal(x, y, z)
 
 if __name__ == "__main__":
     navigator = SendGoal()
