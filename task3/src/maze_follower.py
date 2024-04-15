1cd9ad059889bd76931ec18fb518d0b52a0e9df7
Krish Panchigar <kpanchigar1@sheffield.ac.uk>
2024-04-15 14:25:16 +0100

    Changed tb3 usage to waffle.py

--- task3/src/maze_follower.py
+++ task3/src/maze_follower.py
@@ -2,49 +2,54 @@
 
 import rospy
 from tb3 import Tb3Move, Tb3Odometry, Tb3LaserScan
-
+import waffle
+import math
 class MazeFollower(object):
     def __init__(self):
-        self.tb3_move = Tb3Move()
-        self.tb3_odom = Tb3Odometry()
-        self.tb3_laser = Tb3LaserScan()
         self.rate = rospy.Rate(10) # Hz
         self.left_wall_control = 0.0
         self.right_wall_control = 0.0
+        self.motion = waffle.Motion(debug=True)
+        self.lidar = waffle.Lidar(debug=True)
+        self.odom = waffle.Pose(debug=True)
         self.error = 0.08
         self.fwd_vel = 0.15
         self.ang_vel = 0.5
 
+    def stop(self):
+        self.motion.stop()
+
     def run(self):
-        self.tb3_move.set_move_cmd(self.fwd_vel, 0.0)
-        self.tb3_move.publish()
-        self.left_wall_control = self.tb3_laser.left_wall
-        self.right_wall_control = self.tb3_laser.right_wall
+        self.motion.set_velocity(self.fwd_vel, 0.0)
+        self.motion.publish_velocity()
+        self.left_wall_control = self.lidar.subsets.l3
+        self.right_wall_control = self.lidar.subsets.r3
         # Following right wall
         while not rospy.is_shutdown():
-            self.left_wall = self.tb3_laser.left_wall
-            self.right_wall = self.tb3_laser.right_wall
+            self.left_wall = self.lidar.subsets.l3
+            self.right_wall = self.lidar.subsets.r3
             
             # Keeping the turtle within the error
-            while self.tb3_laser.closest_object_position > 0.3 or self.tb3_laser.closest_object_position == 0.0:
+            while self.lidar.subsets.front > 0.3 or math.isnan(self.lidar.subsets.front):
                 if self.right_wall > self.right_wall_control + self.error:
-                    self.tb3_move.set_move_cmd(self.fwd_vel, -self.ang_vel)
+                    self.motion.set_velocity(self.fwd_vel, -self.ang_vel)
                     print('right')
                 elif self.right_wall < self.right_wall_control - self.error:
-                    self.tb3_move.set_move_cmd(self.fwd_vel, self.ang_vel)
+                    self.motion.set_velocity(self.fwd_vel, self.ang_vel)
                     print('left')
                 else:
-                    self.tb3_move.set_move_cmd(self.fwd_vel, 0.0)
+                    self.motion.set_velocity(self.fwd_vel, 0.0)
                     print('fwd')
-                self.tb3_move.publish()
+                self.motion.publish_velocity()
             
-            print(self.tb3_laser.closest_object_position)
+            print(self.lidar.subsets.front)
             
 if __name__ == '__main__':
     rospy.init_node('maze_follower')
     maze_follower = MazeFollower()
     maze_follower.run()
     rospy.spin()
+    rospy.on_shutdown(maze_follower.stop)
         
         
         
