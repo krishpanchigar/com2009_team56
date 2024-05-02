4c7638fe56a6de382d2b88a9af3299b1d6c396a0
Gabriel Plume <gplume02@gmail.com>
2024-05-02 11:10:34 +0100

    Updating from laptop to pc

--- task3/src/maze_follower.py
+++ task3/src/maze_follower.py
@@ -4,6 +4,7 @@ import rospy
 from tb3 import Tb3Move, Tb3Odometry, Tb3LaserScan
 import waffle
 import math
+import time
 class MazeFollower(object):
     def __init__(self):
         self.rate = rospy.Rate(10) # Hz
@@ -14,7 +15,8 @@ class MazeFollower(object):
         self.odom = waffle.Pose(debug=True)
         self.error = 0.08
         self.fwd_vel = 0.15
-        self.ang_vel = math.pi/2
+        self.ang_vel = math.pi/4
+        self.turn_time = 1.3
 
     def stop(self):
         self.motion.stop()
@@ -30,17 +32,20 @@ class MazeFollower(object):
             self.right_wall = min(self.lidar.subsets.r3Array)
             
             # Keeping the turtle within the error
-            while min(self.lidar.subsets.frontArray) > 0.3 or min(self.lidar.subsets.r3Array) < 0.3 or  math.isnan(self.lidar.subsets.front):
-                if self.right_wall > self.right_wall_control + self.error:
-                    self.motion.set_velocity(self.fwd_vel, -self.ang_vel)
-                    print('right')
-                elif self.right_wall < self.right_wall_control - self.error:
-                    self.motion.set_velocity(self.fwd_vel, self.ang_vel)
-                    print('left')
-                else:
-                    self.motion.set_velocity(self.fwd_vel, 0.0)
-                    print('fwd')
+            while min(self.lidar.subsets.frontArray) > 0.4 or  math.isnan(self.lidar.subsets.front):
+                # if self.right_wall > self.right_wall_control + self.error:
+                #     self.motion.set_velocity(self.fwd_vel, -self.ang_vel)
+                #     print('right')
+                # elif self.right_wall < self.right_wall_control - self.error:
+                #     self.motion.set_velocity(self.fwd_vel, self.ang_vel)
+                #     print('left')
+                # else:
+                #     self.motion.set_velocity(self.fwd_vel, 0.0)
+                #     print('fwd')
+                self.motion.set_velocity(self.fwd_vel, 0.0)
+                print('fwd')
                 self.motion.publish_velocity()
+                
             
             #Check for where walls are:
             self.motion.stop()
@@ -49,19 +54,21 @@ class MazeFollower(object):
             if min(self.lidar.subsets.r3Array) > 0.3:
                 print('No right wall detected. Turning right')
                 self.motion.set_velocity(0.0, -self.ang_vel)
-                self.sleep(1)
+                self.motion.publish_velocity()
+                time.sleep(self.turn_time)
             else:
                 print('Right wall detected')
                 if min(self.lidar.subsets.l3Array) > 0.3:
                     print('No left wall detected. Turning left')
                     self.motion.set_velocity(0.0, self.ang_vel)
-                    self.sleep(1)
+                    self.motion.publish_velocity()
+                    time.sleep(self.turn_time)
                 else:
                     print('Left wall detected')
                     print('Dead end')
                     self.motion.set_velocity(0.0, -self.ang_vel)
                     self.motion.publish_velocity()
-                    self.rate.sleep(2)
+                    time.sleep(2*self.turn_time)
                     break
             
             self.motion.stop()
