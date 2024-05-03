b933dae932196c27ce121ccdcd9feab893330041
Krish Panchigar <krishpanchigar14@gmail.com>
2024-05-03 22:26:45 +0100

    added some functions in maze follower

--- task3/src/maze_follower.py
+++ task3/src/maze_follower.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3 
 
+from distutils.log import error
 import rospy
 from tb3 import Tb3Move, Tb3Odometry, Tb3LaserScan
 import waffle
@@ -17,7 +18,61 @@ class MazeFollower(object):
         self.fwd_vel = 0.15
         self.ang_vel = math.pi/6
         self.turn_time = 1.3
+        self.right_wall_desired_dist = 0.4
+        self.error = 0.05
     
+    def stop(self):
+        self.motion.stop()
+        self.motion.publish_velocity()
+        time.sleep(1)
+
+    def moveForward(self):
+        front_dist = min(self.lidar.subsets.frontArray)
+        while front_dist > 0.4:
+            print("Going forward")
+            self.motion.set_velocity(self.fwd_vel, 0)
+            self.motion.publish_velocity()
+        self.stop()
+        time.sleep(1)
+
+    def adjustToRightWall(self):
+        right_wall_dist = min(self.lidar.subsets.r3Array)
+
+        if right_wall_dist < self.right_wall_desired_dist - self.error:
+            # if robot is close to the wall, turn left
+            self.motion.set_velocity(0.0, self.ang_vel)
+        elif right_wall_dist > self.right_wall_desired_dist + self.error:
+            # if robot is far from right wall, turn right
+            self.motion.set_velocity(0.0, -self.ang_vel)
+        else:
+            self.motion.set(self.fwd_vel, 0.0)
+        
+        self.motion.publish_velocity()
+
+    def turn_90(self, direction):
+        current_yaw = self.odom.yaw
+
+        if direction == "right":
+            target_yaw = current_yaw - math.pi / 2
+            ang_vel = -self.ang_vel
+        elif direction == "left":
+            target_yaw = current_yaw + math.pi / 2
+            ang_vel = self.ang_vel
+
+        # normalising the target value between -pi and pi
+        while target_yaw > math.pi:
+            target_yaw -= 2 * math.pi
+        while target_yaw < -math.pi:
+            target_yaw += 2 * math.pi
+
+        while abs(self.odom.yaw - target_yaw) > 0.01:
+            self.motion.set_velocity(0.0, ang_vel)
+            self.motion.publish_velocity()
+            self.rate.sleep()
+        
+        self.motion.stop()
+        self.motion.publish_velocity()
+
     def turn(self, angle):
         self.motion.set_velocity(0.0, angle)
         self.motion.publish_velocity()
@@ -83,9 +138,6 @@ class MazeFollower(object):
                 self.motion.publish_velocity()
             
         
-            
-        
-            
 if __name__ == '__main__':
     rospy.init_node('maze_follower')
     maze_follower = MazeFollower()
