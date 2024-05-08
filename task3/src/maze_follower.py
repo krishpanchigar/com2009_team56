c5d45934d586919c38cfe8cb8a908a0d66a11db6
Krish Panchigar <kpanchigar1@sheffield.ac.uk>
2024-05-08 21:32:41 +0100

    tried PID controlling for task3

--- task3/src/maze_follower.py
+++ task3/src/maze_follower.py
@@ -9,9 +9,11 @@ from tb3 import Tb3Move, Tb3Odometry, Tb3LaserScan
 import waffle
 import math
 import time
+
+
 class MazeFollower(object):
     def __init__(self):
-        self.rate = rospy.Rate(10) # Hz
+        self.rate = rospy.Rate(10)  # Hz
         self.left_wall_control = 0.0
         self.right_wall_control = 0.0
         self.motion = waffle.Motion(debug=True)
@@ -19,12 +21,13 @@ class MazeFollower(object):
         self.odom = waffle.Pose(debug=True)
         self.error = 0.08
         self.fwd_vel = 0.2
-        self.ang_vel = math.pi/6
+        self.ang_vel = math.pi / 6
         self.turn_time = 1.3
         self.right_wall_desired_dist = 0.4
         self.error = 0.05
         self.intended_direction = self.odom.yaw * self.odom.yaw_direction
-    
+        self.pid_controller = waffle.PIDController(P=0.2, I - 0.0, D=0.1)
+
     def stop(self):
         self.motion.set_velocity(self.fwd_vel, 0.0)
         self.motion.publish_velocity()
@@ -35,7 +38,7 @@ class MazeFollower(object):
         left_wall = min(self.lidar.subsets.l3Array)
         right_wall = min(self.lidar.subsets.r3Array)
         if right_wall < 0.4 and left_wall < 0.4:
-            average_dist = (left_wall + right_wall) / 2 
+            average_dist = (left_wall + right_wall) / 2
 
             error_left = average_dist - left_wall
             error_right = average_dist - right_wall
@@ -49,10 +52,9 @@ class MazeFollower(object):
                 correction = 0.25 - left_wall
             else:
                 correction = 0
-        
-        self.motion.set_velocity(self.fwd_vel, correction/1.5)
-        self.motion.publish_velocity()
 
+        self.motion.set_velocity(self.fwd_vel, correction / 1.5)
+        self.motion.publish_velocity()
 
     def moveForward(self):
         front_dist = min(self.lidar.subsets.frontArray)
@@ -63,13 +65,12 @@ class MazeFollower(object):
         self.stop()
         time.sleep(1)
         return
-    
+
     def face_wall(self):
         sign = np.sign((self.odom.yaw * self.odom.yaw_direction) - self.intended_direction)
         while abs((self.odom.yaw * self.odom.yaw_direction) - self.intended_direction) > 0.5:
             self.motion.set_velocity(0.0, (sign * self.ang_vel))
             self.motion.publish_velocity()
-        
 
     def turn_90(self, direction):
         # TODO Fix turning logic!!
@@ -80,7 +81,7 @@ class MazeFollower(object):
         print(f"Rounded yaw: {rounded_yaw}")
 
         if direction == "right":
-            
+
             target_yaw = rounded_yaw - 90
             ang_vel = -self.ang_vel
         elif direction == "left":
@@ -88,16 +89,26 @@ class MazeFollower(object):
             ang_vel = self.ang_vel
 
         print(f"Target yaw: {target_yaw}")
-        
 
         while abs((self.odom.yaw * self.odom.yaw_direction) - target_yaw) > 0.5:
             print(self.odom.yaw, self.odom.yaw_direction)
             self.motion.set_velocity(0.0, ang_vel)
             self.motion.publish_velocity()
-        
+
         self.stop()
         self.intended_direction = target_yaw
-            
+
+    def pid_correction(self):
+        left_wall = min(self.lidar.subsets.l3Array)
+        right_wall = min(self.lidar.subsets.r3Array)
+        average_dist = (left_wall + right_wall) / 2
+
+        self.pid_controller.SetPoint = average_dist
+        self.pid_controller.update(right_wall)
+
+        correction = self.pid_controller.output
+        self.motion.set_velocity(self.fwd_vel, correction)
+        self.motion.publish_velocity()
 
     def run(self):
         self.moveForward()
@@ -121,15 +132,12 @@ class MazeFollower(object):
                 self.moveForward()
             # if there is no wall in the front, and there is a right wall, keep going forward
             else:
-                self.moveForward()
-                
-        
+                self.pid_correction()
+
+
 if __name__ == '__main__':
     rospy.init_node('maze_follower')
     maze_follower = MazeFollower()
     maze_follower.run()
     rospy.spin()
     rospy.on_shutdown((maze_follower.motion.stop(), maze_follower.motion.publish_velocity()))
-        
-        
-        
--- task3/src/waffle.py
+++ task3/src/waffle.py
@@ -14,8 +14,8 @@ from tf.transformations import euler_from_quaternion
 from math import degrees
 import numpy as np
 
-class Motion():
 
+class Motion():
     """
     Creates a `/cmd_vel` publisher and has callable methods to control velocity
 
@@ -23,16 +23,16 @@ class Motion():
         Set `debug=True/False` to enable/disable ROS log/warn messages (default = `True`) 
     """
 
-    def __init__(self, debug = True):
+    def __init__(self, debug=True):
         self.dbg = debug
         self.publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-        self.publisher_rate = rospy.Rate(10) # Hz
+        self.publisher_rate = rospy.Rate(10)  # Hz
         self.vel_cmd = Twist()
         self.timestamp = rospy.get_time()
-        self.high_rate_warn = 1.0 / 1000.0 # seconds
-        self.low_rate_warn = 1.0 / 2.0 # seconds
+        self.high_rate_warn = 1.0 / 1000.0  # seconds
+        self.low_rate_warn = 1.0 / 2.0  # seconds
 
-    def set_velocity(self, linear = 0.0, angular = 0.0):
+    def set_velocity(self, linear=0.0, angular=0.0):
         """
         Creates a `Twist()` message with the specified velocities:
         `linear = Twist().linear.x`, default = 0.0 [Units: m/s]
@@ -47,10 +47,10 @@ class Motion():
             ang_org = angular
             angular = np.sign(angular) * 1.82
             if self.dbg: rospy.logwarn(f"ANGULAR velocity limited to {angular} rad/s ({ang_org} rad/s requested).")
-        
+
         self.vel_cmd.linear.x = linear
         self.vel_cmd.angular.z = angular
-        
+
     def stop(self):
         """
         Creates a Twist() message with all velocities set to zero
@@ -68,18 +68,19 @@ class Motion():
 
             Motion().set_velocity(linear= , angular= )
         """
-        last_published = rospy.get_time() - self.timestamp 
+        last_published = rospy.get_time() - self.timestamp
         self.timestamp = rospy.get_time()
         self.publisher.publish(self.vel_cmd)
         if last_published < self.high_rate_warn:
             if self.dbg: rospy.logwarn("You're sending velocity commands too quickly!\n" \
                                        "Check your loop rates!")
         if last_published > self.low_rate_warn:
-            if self.dbg: rospy.logwarn("You aren't publishing velocity commands quickly enough to ensure smooth motion!\n" \
-                                       "Check your loop rates!")
+            if self.dbg: rospy.logwarn(
+                "You aren't publishing velocity commands quickly enough to ensure smooth motion!\n" \
+                "Check your loop rates!")
+
 
 class Pose():
-
     """
     Creates an `/odom` subscriber and handles the robot's `Odometry` data
 
@@ -94,7 +95,7 @@ class Pose():
             [orientation.x, orientation.y, orientation.z, orientation.w],
             'sxyz'
         )
-        
+
         yaw = self._round(degrees(yaw), 4)
         self.yaw_direction = np.sign(yaw)
         self.yaw = abs(yaw)
@@ -102,10 +103,12 @@ class Pose():
         self.posy = self._round(position.y, 4)
 
         self.wait_for_odom = False
-    
-    def __init__(self, debug = True):
+
+    def __init__(self, debug=True):
         self.dbg = debug
-        self.posx = 0.0; self.posy = 0.0; self.yaw = 0.0
+        self.posx = 0.0;
+        self.posy = 0.0;
+        self.yaw = 0.0
         self.yaw_direction = 0.0
         self.subscriber = rospy.Subscriber('/odom', Odometry, self.odom_cb)
         self.timestamp = rospy.get_time()
@@ -114,7 +117,7 @@ class Pose():
         while self.wait_for_odom:
             continue
         if self.dbg: rospy.loginfo('Odometry Data is available...')
-    
+
     def show(self):
         """
         A method to return the robot's current odometry.
@@ -129,13 +132,13 @@ class Pose():
                 f"posx = {self.posx:.3f} (m) posy = {self.posy:.3f} (m), yaw = {self.yaw:.1f} (degrees)\n" \
                 "---"
             )
-        
+
     def _round(self, value, precision):
-        value = int(value * (10**precision))
-        return float(value) / (10**precision)
+        value = int(value * (10 ** precision))
+        return float(value) / (10 ** precision)
+
 
 class Lidar():
-
     """
     Creates a `/scan` subscriber and handles `LaserScan` data from the Waffle's LiDAR sensor
 
@@ -143,7 +146,7 @@ class Lidar():
         Set `debug=True/False` to enable/disable ROS log/warn messages (default = `True`)
     """
 
-    def __init__(self, debug = True):
+    def __init__(self, debug=True):
         self.dbg = debug
         self.subsets = self.scanSubsets()
         self.subscriber = rospy.Subscriber('/scan', LaserScan, self.laserscan_cb)
@@ -152,16 +155,29 @@ class Lidar():
         while self.wait_for_lidar:
             continue
         if self.dbg: rospy.loginfo('LiDAR Data is available...')
-    
+
     class scanSubsets():
         def __init__(self):
             self.timestamp = rospy.get_time()
-            self.front = 0.0; self.frontArray = []
-            self.r1 = 0.0; self.r2 = 0.0; self.r3 = 0.0; self.r4 = 0.0
-            self.l1 = 0.0; self.l2 = 0.0; self.l3 = 0.0; self.l4 = 0.0
-            self.r1Array = []; self.r2Array = []; self.r3Array = []; self.r4Array = []
-            self.l1Array = []; self.l2Array = []; self.l3Array = []; self.l4Array = []
-        
+            self.front = 0.0;
+            self.frontArray = []
+            self.r1 = 0.0;
+            self.r2 = 0.0;
+            self.r3 = 0.0;
+            self.r4 = 0.0
+            self.l1 = 0.0;
+            self.l2 = 0.0;
+            self.l3 = 0.0;
+            self.l4 = 0.0
+            self.r1Array = [];
+            self.r2Array = [];
+            self.r3Array = [];
+            self.r4Array = []
+            self.l1Array = [];
+            self.l2Array = [];
+            self.l3Array = [];
+            self.l4Array = []
+
         def show(self):
             """
             A function to return distance readings from each LiDAR subset.
@@ -172,15 +188,15 @@ class Lidar():
 
             if (rospy.get_time() - self.timestamp) > 1:
                 msg = "LiDAR Subset Readings (meters):\n" \
-                    f"              l1     front     r1          \n" \
-                    f"       l2     {self.l1:<5.3f}  {self.front:^5.3f}  {self.r1:>5.3f}     r2       \n" \
-                    f"l3     {self.l2:<5.3f}                       {self.r2:>5.3f}   r3\n" \
-                    f"{self.l3:<5.3f}                                   {self.r3:>5.3f}\n" \
-                    f"{self.l4:<5.3f} <-- l4                     r4 --> {self.r4:>5.3f}\n" \
-                    "---"
+                      f"              l1     front     r1          \n" \
+                      f"       l2     {self.l1:<5.3f}  {self.front:^5.3f}  {self.r1:>5.3f}     r2       \n" \
+                      f"l3     {self.l2:<5.3f}                       {self.r2:>5.3f}   r3\n" \
+                      f"{self.l3:<5.3f}                                   {self.r3:>5.3f}\n" \
+                      f"{self.l4:<5.3f} <-- l4                     r4 --> {self.r4:>5.3f}\n" \
+                      "---"
                 self.timestamp = rospy.get_time()
                 print(msg)
-                        
+
     def filter(self, lidar_subset):
         valid_data = lidar_subset[(lidar_subset > 0.1) & (lidar_subset != float("inf"))]
         subset_array = valid_data.tolist()
@@ -188,30 +204,90 @@ class Lidar():
         return subset_value, subset_array
 
     def get_subset(self, start_index, stop_index):
-        subset = np.array(self.rangesArray[start_index: stop_index+1])
+        subset = np.array(self.rangesArray[start_index: stop_index + 1])
         return self.filter(subset)
-    
+
     def laserscan_cb(self, scan_data: LaserScan):
-        
+
         self.rangesArray = scan_data.ranges
 
         ## Get LiDAR Subsets:
         # Front:
-        left = scan_data.ranges[0:20+1]
+        left = scan_data.ranges[0:20 + 1]
         right = scan_data.ranges[-20:]
         left_right = np.array(left[::-1] + right[::-1])
         self.subsets.front, self.subsets.frontArray = self.filter(left_right)
-        
+
         # Right (x4):
         self.subsets.r1, self.subsets.r1Array = self.get_subset(320, 340)
         self.subsets.r2, self.subsets.r2Array = self.get_subset(300, 320)
         self.subsets.r3, self.subsets.r3Array = self.get_subset(275, 290)
         self.subsets.r4, self.subsets.r4Array = self.get_subset(250, 265)
-        
+
         # Left (x4):
         self.subsets.l1, self.subsets.l1Array = self.get_subset(20, 40)
         self.subsets.l2, self.subsets.l2Array = self.get_subset(40, 60)
         self.subsets.l3, self.subsets.l3Array = self.get_subset(70, 85)
         self.subsets.l4, self.subsets.l4Array = self.get_subset(95, 110)
-        
+
         self.wait_for_lidar = False
+
+
+class PIDController:
+    def __init__(self, P=0.2, I=0.0, D=0.1):
+        self.Kp = P
+        self.Ki = I
+        self.Kd = D
+        self.sample_time = 0.00
+        self.current_time = time.time()
+        self.last_time = self.current_time
+        self.clear()
+
+    def clear(self):
+        self.SetPoint = 0.0
+        self.PTerm = 0.0
+        self.ITerm = 0.0
+        self.DTerm = 0.0
+        self.last_error = 0.0
+        self.int_error = 0.0
+        self.windup_guard = 20.0
+        self.output = 0.0
+
+    def update(self, feedback_value):
+        error = self.SetPoint - feedback_value
+        self.current_time = time.time()
+        delta_time = self.current_time - self.last_time
+        delta_error = error - self.last_error
+
+        if (delta_time >= self.sample_time):
+            self.PTerm = self.Kp * error
+            self.ITerm += error * delta_time
+
+            if (self.ITerm < -self.windup_guard):
+                self.ITerm = -self.windup_guard
+            elif (self.ITerm > self.windup_guard):
+                self.ITerm = self.windup_guard
+
+            self.DTerm = 0.0
+            if delta_time > 0:
+                self.DTerm = delta_error / delta_time
+
+            self.last_time = self.current_time
+            self.last_error = error
+
+            self.output = self.PTerm + (self.Ki * self.ITerm) + (self.Kd * self.DTerm)
+
+    def setKp(self, proportional_gain):
+        self.Kp = proportional_gain
+
+    def setKi(self, integral_gain):
+        self.Ki = integral_gain
+
+    def setKd(self, derivative_gain):
+        self.Kd = derivative_gain
+
+    def setWindup(self, windup):
+        self.windup_guard = windup
+
+    def setSampleTime(self, sample_time):
+        self.sample_time = sample_time
